6e9d968d09cd819c9d435ae085a4afc0ea1f9e06
JBatUN <jaybellone@hotmail.com>
2021-12-05 23:34:16 -0500

    Session prep

--- sources/z-scripts/create_production_output.R
+++ sources/z-scripts/create_production_output.R
@@ -23,6 +23,7 @@ full_path <- gsub(".Rmd", "", full_path)
 
 # Create relative path
 relative_path <- sub(".*sources/", "", full_path)
+print(paste0("the relative path is: ", relative_path))
 
 # Use path as unique id, make sure file names conform to standard (hyphenate spaces)
 id <- gsub("/", "-", relative_path)
@@ -42,50 +43,56 @@ html_file <- paste0(file, ".html")
 
 destination_path <- file.path("../../../../packages/web/content/", taxonomy$activity, taxonomy$subactivity, taxonomy$task)
 destination_file <- file.path(destination_path, "/", html_file)
+print(paste0("the destination path is: ", destination_file))
 
 # file.info(as.character(rmd_file))
 
 # check if the source file exists
 if ( file.exists(as.character(html_file)) ) {
   
-  #print("file exists")
+  print("a source html file exists")
   
   # check if destination folder exists
   if (!dir.exists(destination_path) ) {
-    #print("dir does not exist")
+    print("dir does not exist, creating...")
     #print(destination_path)
     dir.create(destination_path, showWarnings = TRUE, recursive = TRUE)
   }
   
-  # if old version exists, delete
+  # if old version exists, overwrite
   if ( file.exists(destination_file) ) {
-    print("old version exists")
+    print("an old version exists, will overwrite")
   }
   
   # now copy file to directory
   # print(html_file)
   file.copy(html_file, destination_file, overwrite = TRUE)
+  print("copying html file...")
   # then delete from source
   file.remove(html_file)
+  print("removing source html file...")
   
   # now copy assets files
   # masthead
   file.copy(masthead, paste0(destination_path, sub(".*assets", "", masthead)), overwrite = TRUE)
-
+  print("copying masthead file...")
 }
 
 # STEP 2: FIND AND REPLACE PATH TEXT
+print("replacing paths...")
 tx  <- readLines(destination_file)
 tx1  <- gsub(pattern = "../../../z-assemblers/", replace = "../../../../", x = tx)
 tx2  <- gsub(pattern = "vendor/", replace = "../../../../vendor/knitr/", x = tx1)
 tx3  <- gsub(pattern = masthead, replace = sub(".*assets/", "", masthead), x = tx2)
-<<<<<<< HEAD
+
+#style_sheet <- '<link rel="stylesheet" href="../../../../assets/styles/content.css" type="text/css" />'
+#tx4  <- gsub(pattern = ".*content.css.*", replace = style_sheet, x = tx3)
 
 print("rewriting html file...")
-=======
->>>>>>> parent of bb67a36 (initial maps commit)
 writeLines(tx3, con=destination_file)
 
 
 # STEP 3: REMOVE VENDOR FOLDER
+print("removing vendor folder...")
 unlink(c("vendor"), recursive = TRUE, force = FALSE, expand = TRUE)
+print("done.")
